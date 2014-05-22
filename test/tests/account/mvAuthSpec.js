5cf51d54fea2411e3e940ff3ebbb47148ab0726b
John Chynoweth <JChynoweth@vmdsystems.com>
2014-05-22 00:49:56 -0400

    baseline

---
+++ test/tests/account/mvAuthSpec.js
@@ -0,0 +1,40 @@
+﻿describe( 'mvAuth', function () {
+    var mvAuth, mvIdentity;
+
+    beforeEach( module( 'app' ) );
+    
+    beforeEach( inject( function ( $window ) {
+        $window.bootstrappedUserObject = {password: 'pswrdxx', firstName: 'Johnxx', lastName: 'Chynowethxx', username: 'before', roles: ['xx'] };
+        
+    }) );
+
+     beforeEach(inject(function(_mvAuth_, _mvIdentity_) {
+
+         mvAuth = _mvAuth_;
+         mvIdentity = _mvIdentity_;
+         
+
+          var newUserData = {
+                username: 'jchynwoeth',
+                password: 'pswrd',
+                firstName: 'John',
+                lastName: 'Chynoweth'
+            };
+         mvAuth.createUser(newUserData);
+        
+
+     }) );
+
+
+    it('should be able to add user', function(done) {
+        
+        done();
+        expect(mvIdentity.currentUser.username).equal("jchynwoeth");
+        
+    });
+
+
+
+});
+
+
