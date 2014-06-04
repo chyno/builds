774517401d4cf32b2201965818a732e3962e65a4
John Chynoweth <JChynoweth@vmdsystems.com>
2014-06-04 18:31:40 -0400

    add unit testing

---
+++ TfsBuildsConnectorTest/BuildsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TFSBuildsConnector;
+
+namespace TfsBuildsConnectorTest
+{
+    [TestClass]
+    public class BuildsTest
+    {
+        [TestMethod]
+        public void CantGetBuild()
+        {
+            //Arrange
+            var tfsBuild = new Startup();
+
+            //Act
+            Task<object> bldsTask = tfsBuild.Invoke("input");
+            var result = bldsTask.Result;
+
+            //Assert
+            Assert.IsNotNull(result);
+
+
+        }
+    }
+}
